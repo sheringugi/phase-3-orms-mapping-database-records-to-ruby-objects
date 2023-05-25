fd33fb71a48ea91eb74d9027b9e3cba2dcc44102
sheringugi <sheri.ngugi17@gmail.com>
2023-05-25 11:06:59 +0300

    complete assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+# require_relative "../config/environment"
+
 class Song
 
   attr_accessor :name, :album, :id
@@ -7,6 +9,7 @@ class Song
     @name = name
     @album = album
   end
+  
 
   def self.drop_table
     sql = <<-SQL
@@ -48,5 +51,39 @@ class Song
     song = Song.new(name: name, album: album)
     song.save
   end
+
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+    
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new(id: row[0], name: row[1], album: row[2])
+
+
+    end.first
+  end
+
+
 
 end
